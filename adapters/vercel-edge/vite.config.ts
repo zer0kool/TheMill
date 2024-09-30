b8a2be9dbcda4ac4fe7f63aceb01a1766c64cd43
zer0kool <dwebcrew@protonmail.ch>
2024-09-29 20:38:05 -0700

    test

--- adapters/vercel-edge/vite.config.ts
+++ adapters/vercel-edge/vite.config.ts
@@ -2,15 +2,27 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { vercelEdgeAdapter } from '@builder.io/qwik-city/adapters/vercel-edge/vite';
+import baseConfig from '../../vite.config';
+import { resolve } from 'path';
 
 export default defineConfig(() => {
   return {
+    ...baseConfig,
+    build: {
+      ssr: true,
+      rollupOptions: {
+        input: ['src/entry.vercel-edge.tsx', '@qwik-city-plan'],
+      },
+    },
     plugins: [
       qwikCity(),
       qwikVite(),
       tsconfigPaths(),
-      vercelEdgeAdapter(),
     ],
+    resolve: {
+      alias: {
+        '~': resolve(__dirname, '../../src'),
+      },
+    },
   };
 });
