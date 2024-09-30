3f3ca4975bb4874723892ded20cb234be2102ec0
zer0kool <dwebcrew@protonmail.ch>
2024-09-29 18:56:22 -0700

    omit

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
 /**
  * Note that Vite normally starts from `index.html` but the qwikCity plugin makes start at `src/entry.ssr.tsx` instead.
  */
-export default defineConfig(({ command, mode }): UserConfig => {
+export default defineConfig(() => {
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     // This tells Vite which dependencies to pre-build in dev mode.
